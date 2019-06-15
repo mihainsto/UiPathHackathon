8d3f1396c65074f9410136a840e6e5e83f85a1f7
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-06-15 16:13:11 +0300

    Add comment rating visualisation code

---
+++ stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+TONES = [
+    'Happy',
+    'Confident',
+    'Tentative',
+    'Analytical',
+    'Sad',
+    'Fearful',
+    'Angry',
+]
+
+GOOD_TONES = TONES[:3]
+BAD_TONES = TONES[4:]
+
+def reindex_tones(df):
+    print(df.index)
+    return df
+
+if __name__ == '__main__':
+    INPUT_CSV = 'comments.csv'
+
+    df = pd.read_csv(INPUT_CSV)
+
+    grouped = df.groupby(['Tone'], sort=False)
+
+    summed = grouped.sum()
+    counted = grouped.count()
+
+    counted.plot.pie(y='Confidence')
+    plt.title("YouTube Comment Sentiments")
+    plt.show()
